d08fbaa30a87fdc1107f3f31b5a69f93dad97525
SwoftCreation <limsoft0401@gmail.com>
2022-07-27 12:23:14 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
+import { useState } from 'react';
+
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트의 기초 알아보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링 해보기', checked: true },
+    { id: 3, text: '일정관리 앱 만들어보기', checked: false },
+  ]);
+
   return (
     <div>
       <TodoTemplate>
         <TodoInsert />
-        <TodoList />
+        <TodoList todos={todos} />
       </TodoTemplate>
     </div>
   );
